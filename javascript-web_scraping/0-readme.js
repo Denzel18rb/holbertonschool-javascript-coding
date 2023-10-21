a303a2d96066ef14c72e3e43ea895a2dee7839e3
Denzel18rb <74129875rb@gmail.com>
2023-10-21 13:54:00 -0500

    corrected error

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,21 +1,12 @@
 #!/usr/bin/node
-// Import the 'fs' module to work with the file system
+
+const file = process.argv[2];
 const fs = require('fs');
 
-// Check if the correct number of command line arguments is provided
-if (process.argv.length !== 3) {
-  console.error('Usage: node 0-readme.js <file_path>');
-  process.exit(1); // Exit the program with an error code
-}
-
-// Get the file path from the command line argument
-const filePath = process.argv[2];
-
-// Read the content of the file in utf-8 encoding
-fs.readFile(filePath, (err, data) => {
-  if (err) {
-    console.error(err); // Display the error if one occurs
+fs.readFile(file, 'utf8', (error, data) => {
+  if (error) {
+    console.log(error);
   } else {
-    console.log(data.toString()); // Display the file content if there are no errors
+    console.log(data);
   }
 });
