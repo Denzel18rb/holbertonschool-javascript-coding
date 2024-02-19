26f6b3788017e9b517a3dc1a3c8c2ca0f3ad8c86
Denzel18rb <74129875rb@gmail.com>
2024-02-19 09:48:40 -0500

    tests a file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,12 +2,6 @@
 
 const fs = require('fs');
 
-// Check if the file path is provided as a command-line argument
-if (process.argv.length !== 3) {
-  console.error('Usage: node readfile.js <file_path>');
-  process.exit(1);
-}
-
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', (err, data) => {
