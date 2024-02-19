afa6ee049f1dde61fca1360042fba386574886fa
Denzel18rb <74129875rb@gmail.com>
2024-02-19 09:49:49 -0500

    return a file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,6 +2,12 @@
 
 const fs = require('fs');
 
+// Check if the file path is provided as a command-line argument
+if (process.argv.length !== 3) {
+  console.error('Usage: node readfile.js <file_path>');
+  process.exit(1);
+}
+
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', (err, data) => {
